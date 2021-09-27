ee26540bb6ae8af1c8513403c88c043aeae3799a
JHpusd <69809321+JHpusd@users.noreply.github.com>
2021-09-27 22:16:37 +0000

    finished move_off_board strategy

---
+++ tests/test_move_off_board.py
@@ -0,0 +1,13 @@
+import sys
+sys.path[0] = '/home/runner/space-empires-2'
+from game import *
+from player import *
+sys.path.append('strategies')
+from move_strat import *
+from move_off import *
+
+players = [Player(MoveToClosestCol()), Player(MoveOff())]
+game = Game(players, log_name='test_move_off_tests.txt')
+
+game.run_to_completion()
+print(game.winner)
