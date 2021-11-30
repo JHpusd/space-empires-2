0fb590f334e128aa9734a39a82c7bf4147cbddbe
JHpusd <justinhongdebate@gmail.com>
2021-11-30 23:16:25 +0000

    battleship stuff

--- ship_info.py
+++ ship_info.py
@@ -6,4 +6,4 @@ battleship = {'name':'Battleship','hp':3,'atk':5,'df':2,'ship_class':"A",'cp_cos
 cruiser = {'name':"Cruiser",'hp':2,'atk':4,'df':1,'ship_class':"C",'cp_cost':12,'obj':Cruiser}
 destroyer = {'name':"Destroyer",'hp':1,'atk':4,'df':0,'ship_class':"D",'cp_cost':9,'obj':Destroyer}
 dreadnaught = {'name':"Dreadnaught",'hp':3,'atk':6,'df':3,'ship_class':"A",'cp_cost':24,'obj':Dreadnaught}
-all_ship_infos = [scout, battlecruiser, cruiser, destroyer, dreadnaught]
+all_ship_infos = [scout, battlecruiser, battleship, cruiser, destroyer, dreadnaught]
